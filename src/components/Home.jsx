dc29bb42525291a15a35bcd5390ec55a5b7ef2c5
ryk1mmy <kimrelace@gmail.com>
2023-04-15 12:51:45 +0300

    logo 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/kimn.pnggit ";
+import HeroImage from "../assets/kimn.png ";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
