1cf8e9bca3561153fa1592d4f457b2ad341c95f1
ryk1mmy <kimrelace@gmail.com>
2023-04-24 21:37:35 +0300

    changes on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.png";
+import HeroImage from "../assets/heroImage.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
